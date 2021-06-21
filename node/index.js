578beb15df34287940461701ad5f3419aee330b8
Marcelo Magrinelli <marcelovbm@gmail.com>
2021-06-21 12:36:17 -0300

    first commit

--- node/index.js
+++ node/index.js
@@ -1,10 +1,40 @@
 import express from 'express';
+import { createConnection } from 'mysql';
 const app = express()
 const PORT = 3000
+const connection = createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+})
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('INSERT INTO `people`(`name`) VALUES(?)', ['Magrinelli'], function (error, results, fields) {
+    if (error) throw error
+    console.log(`Dados inseridos: ${JSON.stringify(results)} - ${JSON.stringify(fields)}`)
+})
 
 app.get('/', (request, response) => {
-    response.statusCode = 200
-    response.send(`<h1>Full Cycle Rocks!</h1>`)
+
+    connection.query('SELECT `name` FROM `people`', function (error, results, fields) {
+        if (error) throw error
+
+        const retorno = `<h1>Full Cycle Rocks!</h1> <ul>${results.map(result => {
+            return `<li>${result.name}</li>`
+        }).join('')}</ul>`
+
+        console.log(retorno)
+        response.status(200)
+        response.send(retorno)
+    })
 })
 
 app.listen(PORT, () => {
